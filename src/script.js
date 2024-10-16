c06024127c631e98673d82671f4ad8a3d06732ba
ELAINE CRISTINA DE AMORIM <elaine_c_amorim@estudante.sesisenai.org.br>
2024-10-15 21:55:12 -0300

    avaliacao

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
-alert("Script conectado a página")
+function adicionarVeiculo(){
+    const imput = document.getElementById(adicionarVeiculo);
+    const adicionarVeiculo = input.value.trim();
+    
+    }
+
+
+function adicionarnome(){
+    const imput = document.getElementById(adicionarnome);
+    const adicionarnome = input.value.trim();
+}
+
+const nome = document.getElementById('nome').value;
+const ano = document.getElementById('ano').value;
+const imagem = document.getElementById('imagem').value;
+
+
+   
+    
+
+    
+   
+
+
+
